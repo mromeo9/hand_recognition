ac33431df255c6c50dbd2f5e4e61654c39cb4778
mromeo9 <121847397+mromeo9@users.noreply.github.com>
2024-07-06 13:29:07 +1000

    Camera Access created

---
+++ src/components/camera_access.py
@@ -0,0 +1,52 @@
+# Importing required libraries 
+import sys
+import cv2 as cv
+
+# Importing exceptions and logging 
+from src.exceptions import CustomException
+from src.logger import logging
+
+class Camera():
+    """
+    Camera class to create an object to acces the camera
+    """
+
+    def __call__(self, camera_path = None):
+        """
+        On call the camera object will access the camera
+        """
+        try:
+            logging.info("Trying to access the camera")
+
+            if camera_path is None:
+                logging.info("Camera path not given")
+
+            else:
+                cam = cv.VideoCapture(camera_path)
+
+                if not cam.isOpened():
+                    logging.info("Could not access camera")
+
+                else:
+                    logging.info("Camera accessed successfully")
+                    
+                    while cam.isOpened():
+                        
+                        #Access the frame 
+                        ret, frame = cam.read()
+                        cv.imshow('Camera', frame)
+
+                        if cv.waitKey(1) & 0xFF == ord('q'):
+                            logging.info("Closing camera")
+                            break
+                    
+                    cam.release()
+                    cv.destroyAllWindows()
+
+        except CustomException as e:
+            raise CustomException(e,sys)
+
+if __name__ == "__main__":
+
+    camera = Camera()
+    camera(1)
